afc6cc3495107369994aa0196569daeebbcd66dc
kanamonra <ceceg@inha.edu>
2023-02-03 13:29:01 +0900

    pokemon v.01.6 last

--- main.py
+++ main.py
@@ -1,81 +1,68 @@
-# pokemon v.01.5
+# pokemon v.01.6
 
-
-pokemons = ["Pikachu", "Eevee", "Dragonite", "Krogre", "Palkia"]
+def add_data(pokemon):
+    pokemon.append(None)
+    pLen = len(pokemon)
+    pokemon[pLen - 1] = pokemon
 
 
 def insert_data(idx, pokemon):
     if idx < 0 or idx > len(pokemons):
-        print("Out of range")
+        print("OUT OF SPACE!")
         return
 
     pokemons.append(None)
+    kLen = len(pokemons)
 
-    for i in range(len(pokemons) - 1, idx, -1):
+    for i in range(kLen - 1, idx, -1):
         pokemons[i] = pokemons[i - 1]
         pokemons[i - 1] = None
 
-    pokemons[idx] = pokemon
+    pokemons[idx] = pokemon  # 배열의 제일 뒤에 친구 추가
 
-def add_data(pokemon):
-    """
-    making for updated ver for add data in this section
-    :param pokemon: number type
-    :return: nothing
-    """
-    pokemons.append(None)
-    pokemon[len(pokemons) -1] = pokemons
 
-def del_data(idx):
+def delete_data(idx):
     if idx < 0 or idx > len(pokemons):
-        print("Out of range!")
+        print("OUT OF SPACE!")
         return
 
-# long way
-    # pLen = len(pokemons)
-    # pokemons[idx] = None  # del data
-    # for i in range(idx +1 , pLen):
-    #     pokemons[i] = None
-    # for i in range(idx, 7):
-    #     # pokemons[i + 1] = pokemons[i]
-    #     pokemons.pop()
+    pLen = len(pokemons)
+    pokemons[idx] = None  # data del
 
-# short way for delete data
-    for _ in range(len(pokemons)- idx):
-        pokemons.pop()
-    temp = pokemons[:idx]
+    for i in range(idx + 1, pLen):
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None  # 배열의 제일 위치 삭제
 
-    # del pokemons[i]
-
-    # del (pokemons[pLen - 1])
+    del (pokemons[pLen - 1])
 
 
+## list making ##
+pokemons = []
+menu = -1  # 1: Add, 2: Call, 3: Delete, 4: Exit
+
+## main section ##
 if __name__ == "__main__":
-    insert_data(2, 'Eevee')  # instead, there is an way of class.insert to add data
-    # pokmons.insert(2, 'Eevee')   => for example
 
-    print(pokemons)
-    insert_data(6, 'Star')
-    print(pokemons)
+    while True:
 
-    # deleting data
-    del_data(1)
-    print(pokemons)
-    add_data('Ceceg')
-    print(pokemons)
-# pokemons = list()  # Empty array
+        menu = int(input("Please select(1: Add, 2: Call, 3: Delete, 4: Exit)--> "))
 
-
-# def add_data(pokemon):
-#    pokemons.append(None)
-#    # lenPokemons = len(pokemons) // other way to make lenght of array
-#    pokemons[-1] = pokemon
-
-
-# add_data('Pikachu')
-# add_data('Eevee')
-# add_data('Dragonite')
-# add_data('Krogre')
-# add_data('Palkia')
-# 
-# print(pokemons)
+        if (menu == '1'):
+            data = input("Add pokemon name--> ")
+            add_data(data)
+            print(pokemons)
+        elif (menu == '2'):
+            pos = int(input("Where to add--> "))
+            data = input("Additional data--> ")
+            insert_data(pos, data)
+            print(pokemons)
+        elif (menu == '3'):
+            pos = int(input("Which pokemon do you want to delete--> "))
+            delete_data(pos)
+            print(pokemons)
+        elif (menu == '4'):
+            print(pokemons)
+            exit()
+        else:
+            print("Please enter a number between 1-4.")
+            continue
