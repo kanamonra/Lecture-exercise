9aa8a127dcbb890c1076eb50ba59cd9141e42dd6
kanamonra <ceceg@inha.edu>
2023-02-03 11:30:28 +0900

    pokemon v.01.03

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# pokemon v.01.2
+# pokemon v.01.3
 
 
 pokemons = ["Pikachu", "Eevee", "Dragonite", "Krogre", "Palkia"]
@@ -9,25 +9,46 @@ def insert_data(idx, pokemon):
         print("Out of range")
         return
 
-    pokemons.append(None)  # 빈칸 추가
-    pLen = len(pokemons)  # 배열의 현재 크기
+    pokemons.append(None)
 
-    for i in range(pLen - 1, idx, -1):
+    for i in range(len(pokemons) - 1, idx, -1):
         pokemons[i] = pokemons[i - 1]
         pokemons[i - 1] = None
 
-    pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
+    pokemons[idx] = pokemon
 
 
-insert_data(2, 'Palkia')
-print(pokemons)
-insert_data(6, 'Eevee')
-print(pokemons)
+def del_data(idx):
+    if idx < 0 or idx > len(pokemons):
+        print("Out of range!")
+        return
+
+    pLen = len(pokemons)
+    pokemons[idx] = None  # 데이터 삭제
+
+    for i in range(idx + 1, pLen):
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
+
+    del (pokemons[pLen - 1])
+
+
+if __name__ == "__main__":
+    insert_data(2, 'Eevee')  # instead, there is an way of class.insert to add data
+    # pokmons.insert(2, 'Eevee')   => for example
+
+    print(pokemons)
+    insert_data(6, 'Star')
+    print(pokemons)
+
+    # deleting data
+    del_data(5)
+    print(pokemons)
 
 # pokemons = list()  # Empty array
 
 
-#def add_data(pokemon):
+# def add_data(pokemon):
 #    pokemons.append(None)
 #    # lenPokemons = len(pokemons) // other way to make lenght of array
 #    pokemons[-1] = pokemon
