968ec4f68cc18c412d20db7ed8ca75e347b8df1a
kanamonra <ceceg@inha.edu>
2023-02-08 11:32:15 +0900

    day17 chap9 graphDFS friends

--- day18.py
+++ day18.py
@@ -1,5 +1,5 @@
 # Chapter 4
-# Graph
+# Graph for friends that has ...
 
 class graph:
     def __init__(self, size):
@@ -7,40 +7,76 @@ class graph:
         self.graph = [[0 for _ in range(size)] for _ in range(size)]
 
 
-g1 = graph(4)
+def print_graph(g):
+    print('', end='')
+    for v in range(g.SIZE):
+        print(nameAry[v], end=' ')
+    print()
+    for row in range(g.SIZE):
+        print(nameAry[row], end=' ')
+        for col in range(g.SIZE):
+            print(g.graph[row][col], end=' ')
+        print()
+    print()
 
-g1.graph[0][1] = 1
-g1.graph[0][2] = 1
-g1.graph[0][3] = 1
 
-g1.graph[1][0] = 1
-g1.graph[1][2] = 1
-g1.graph[1][3] = 1
+# making g1 and give data
+g1 = None
+nameAry = ['Mon', 'Cha', 'Rac', 'Joy', 'Pho', 'Ros']
+Monica, Chandler, Rachel, Joy, Phoebe, Rose = 0, 1, 2, 3, 4, 5
+stack = []
+visitedAry = []
 
-g1.graph[2][0] = 1
-g1.graph[2][1] = 1
-g1.graph[2][3] = 1
+# main section
+gSize = 6
+g1 = graph(gSize)
 
-g1.graph[3][0] = 1
-g1.graph[3][2] = 1
+g1.graph[Monica][Chandler] = 1
+g1.graph[Monica][Rachel] = 1
+g1.graph[Chandler][Monica] = 1
+g1.graph[Chandler][Joy] = 1
+g1.graph[Rachel][Monica] = 1
+g1.graph[Rachel][Joy] = 1
+g1.graph[Joy][Chandler] = 1
+g1.graph[Joy][Rachel] = 1
+g1.graph[Joy][Phoebe] = 1
+g1.graph[Joy][Rose] = 1
+g1.graph[Phoebe][Joy] = 1
+g1.graph[Phoebe][Rose] = 1
+g1.graph[Rose][Joy] = 1
+g1.graph[Rose][Phoebe] = 1
 
-print('Non direction graph g1')
+print('G1 non direction graph')
+
+
 for row in range(4):
     for col in range(4):
         print(g1.graph[row][col], end=' ')
     print()
 
-# g2 example without looking
-g2 = graph(4)
-g2.graph[0][3] = 1
-g2.graph[1][2] = 1
-g2.graph[1][3] = 1
-g2.graph[3][0] = 1
-g2.graph[3][1] = 1
-g2.graph[2][1] = 1
+print_graph(g1)
+current = 0		# 시작 정점 A
+stack.append(current)
+visitedAry.append(current)
 
-print('Example')
-for row in range(4):
-    for col in range(4):
-        print(g2.graph[row][col], end=' ')
-    print()
+while (len(stack) != 0):
+    next = None
+    for vertex in range(4):
+        if g1.graph[current][vertex] == 1:
+            if vertex in visitedAry:
+                pass
+            else:
+                next = vertex
+                break
+
+    if next != None:
+        current = next
+        stack.append(current)
+        visitedAry.append(current)
+    else:
+        current = stack.pop()
+
+
+print('Visited set -->', end='')
+for i in visitedAry:
+    print(i, end='   ')
