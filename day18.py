fe47e6ed4e52d82b5eb67fb7c1c5d2d12f503cf0
kanamonra <ceceg@inha.edu>
2023-02-08 13:48:24 +0900

    day17 graph BFS _ used deque

--- day18.py
+++ day18.py
@@ -1,5 +1,7 @@
 # Chapter 4
-# Graph for friends that has ...
+# Graph BFS - using deque([])
+from collections import deque
+
 
 class graph:
     def __init__(self, size):
@@ -7,81 +9,44 @@ class graph:
         self.graph = [[0 for _ in range(size)] for _ in range(size)]
 
 
-
-
 # making g1 and give data
-g1 = None
-stack = []
+g = None
+# queue = []
+queue = deque([])
 visited_array = []
-name_array = []
 
 # main section
-SIZE = 9
-g1 = graph(SIZE)
+SIZE = 4
+g = graph(SIZE)
 
-g1.graph[0][1] = 1
-g1.graph[0][2] = 1
-g1.graph[0][4] = 1
-
-g1.graph[1][0] = 1
-g1.graph[1][2] = 1
-g1.graph[1][3] = 1
-
-g1.graph[2][0] = 1
-g1.graph[2][1] = 1
-g1.graph[2][4] = 1
-g1.graph[2][5] = 1
-
-g1.graph[3][1] = 1
-g1.graph[3][2] = 1
-g1.graph[4][0] = 1
-g1.graph[4][2] = 1
-g1.graph[4][6] = 1
-
-g1.graph[5][3] = 1
-
-g1.graph[6][4] = 1
-g1.graph[6][8] = 1
-
-g1.graph[7][4] = 1
-g1.graph[7][8] = 1
-
-g1.graph[8][6] = 1
-g1.graph[8][7] = 1
-
-
-
-print('G1 non direction graph')
-
-
-for row in range(9):
-    for col in range(9):
-        print(g1.graph[row][col], end=' ')
-    print()
-
+g.graph[0][3] = 1
+g.graph[1][2] = 1
+g.graph[1][3] = 1
+g.graph[3][0] = 1
+g.graph[3][1] = 1
+g.graph[2][1] = 1
 
 current = 0
-stack.append(current)
+queue.append(current)
 visited_array.append(current)
 
-while (len(stack) != 0):
-    next = None
-    for vertex in range(9):
-        if g1.graph[current][vertex] == 1:
+while len(queue) != 0:
+    n = None
+    for vertex in range(4):
+        if g.graph[current][vertex] == 1:
             if vertex in visited_array:
                 pass
             else:
-                next = vertex
+                n = vertex
                 break
 
-    if next != None:
-        current = next
-        stack.append(current)
+    if n is not None:
+        current = n
+        queue.append(current)
         visited_array.append(current)
     else:
-        current = stack.pop()
+        current = queue.popleft()
 
-
-print('Visited set -->', end='')
 for i in visited_array:
-    print(chr(ord('A')+i), end='   ')
+    print(i, end=' --> ')
+print('DONE!')
