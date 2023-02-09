3c4c0e2ff8dd987901075673edc00c80fedb6186
kanamonra <ceceg@inha.edu>
2023-02-09 14:26:02 +0900

    day19 chap11 self12-2

--- day19.py
+++ day19.py
@@ -1,27 +1,31 @@
-# Sorting algorithm
-# Insertion sort
+# Sorting algorithm chapter 12
+# Quick sort
 import random
 
+def quickSort(ary):
+    global count_quick
+    n = len(ary)
+    if n <= 1:
+        return ary
 
-def find_insert_idx(ary, data):
-    find_idx = -1
-    for i in range(0, len(ary)):
-        if ary[i] < data:
-            find_idx = i
-            break
-    if find_idx == -1:
-        return len(ary)
-    else:
-        return find_idx
+    pivot = ary[n // 2]
+    leftAry, rightAry, midAry = [], [], []
+
+    for num in ary:
+        if num > pivot:
+            leftAry.append(num)
+        elif num < pivot:
+            rightAry.append(num)
+        else:
+            midAry.append(num)
+
+    count_quick = count_quick + 1
+    return quickSort(leftAry) + [pivot] + quickSort(rightAry)
 
 
-before = [random.randint(0, 200) for _ in range(10)]
-after = []
-
-
-print('Before -->', before)
-for i in range(len(before)):
-    data = before[i]
-    insPos = find_insert_idx(after, data)
-    after.insert(insPos, data)
-print('After -->', after)
+dataAry = [random.randint(0, 200) for _ in range(20)]
+count_quick = 0
+print('Before sort -->', dataAry)
+dataAry = quickSort(dataAry)
+print('After sort', dataAry)
+print("Quick repeat time:", count_quick, end=' ')
