ed65e03756d6f72a9a2300bd25e6890ea912d2b6
kanamonra <ceceg@inha.edu>
2023-02-09 10:51:25 +0900

    day19 chap10 Palindrome

--- day19.py
+++ day19.py
@@ -1,59 +1,23 @@
-# BFS using deque
+# Chapter 10 Recursion
+# Palindrome
 
-from collections import deque
+def palindrome(pStr):
+    if len(pStr) <= 1:
+        return True
 
+    if pStr[0] != pStr[-1]:
+        return False
 
-class Graph:
-    def __init__(self, size):
-        self.SIZE = size
-        self.graph = [[0 for x in range(size)] for x in range(size)]
-
-
-def BFS(g):
-    queue = deque([])
-    visited_array = []
-    start = 0
-    queue.append(start)
-    visited_array.append(start)
-
-    while queue:
-        current = queue.popleft()
-        for vertex in range(g.SIZE):
-            if g.graph[current][vertex] == 1 and vertex not in visited_array:
-                queue.append(vertex)
-                visited_array.append(vertex)
-    for i in range(g.SIZE):
-        for j in range(g.SIZE):
-            print(g.graph[i][j], end=' ')
-        print()
-    for i in visited_array:
-        print(chr(ord('A') + i), end=' -> ')
-    print("END")
+    return palindrome(pStr[1:len(pStr) - 1])
 
 
-g = None
-
-A, B, C, D, E, F, G, H, I = 0, 1, 2, 3, 4, 5, 6, 7, 8
-
-g1 = Graph(9)
-g1.graph[A][B] = 1; g1.graph[A][C] = 1; g1.graph[A][E] = 1
-g1.graph[B][A] = 1; g1.graph[B][C] = 1; g1.graph[B][D] = 1
-g1.graph[C][A] = 1; g1.graph[C][B] = 1; g1.graph[C][D] = 1; g1.graph[C][E] = 1; g1.graph[C][F] = 1
-g1.graph[D][B] = 1; g1.graph[D][C] = 1
-g1.graph[E][A] = 1; g1.graph[E][C] = 1; g1.graph[E][G] = 1; g1.graph[E][H] = 1
-g1.graph[F][C] = 1
-g1.graph[G][E] = 1; g1.graph[G][I] = 1
-g1.graph[H][E] = 1; g1.graph[H][I] = 1
-g1.graph[I][G] = 1; g1.graph[I][H] = 1
-
-g2 = Graph(4)
-g2.graph[A][C] = 1; g2.graph[A][D] = 1
-g2.graph[B][C] = 1
-g2.graph[C][A] = 1; g2.graph[C][B] = 1; g2.graph[C][D] = 1
-g2.graph[D][A] = 1; g2.graph[C][B] = 1
+strAry = ["reaver", "kayak", "Borrow or rob", "주유소의 소유주", "야 너 이번주 주번이 너야", "살금 살금"]
 
 
-
-
-BFS(g1)
-BFS(g2)
+for testStr in strAry:
+    print(testStr, end='--> ')
+    testStr = testStr.lower().replace(' ', '')
+    if palindrome(testStr):
+        print('O')
+    else:
+        print('X')
